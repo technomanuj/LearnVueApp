9979d296e4a97bc7102c8b4ed80a3a4f20d6983e
Manuj Sharma <technomanuj@hotmail.com>
2022-03-02 17:22:04 +0530

    update in new branch

--- src/api/users_get.cs
+++ src/api/users_get.cs
@@ -24,13 +24,7 @@ namespace api
             client.BaseAddress=new Uri("https://jsonplaceholder.typicode.com/users");
             var result=await client.GetStringAsync("https://jsonplaceholder.typicode.com/users/");
 
-            // string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            // dynamic data = JsonConvert.DeserializeObject(requestBody);
-            // name = name ?? data?.name;
-
-            // string responseMessage = string.IsNullOrEmpty(name)
-            //     ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-            //     : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            
 
             return new OkObjectResult(result);
         }
